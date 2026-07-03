c339f00063b72d16d0966cba89ee1209a5a1c7ca
Saras Joshi <sarasjjoshi@gmail.com>
2026-07-03 23:08:30 +0530

    fetching datasource in job

--- apps/worker/src/workers/ingestion.worker.ts
+++ apps/worker/src/workers/ingestion.worker.ts
@@ -1,12 +1,22 @@
 import { Worker } from "bullmq";
-
+import { PrismaClient } from "@prisma/client";
 import { redisConfig } from "../config/redis";
+
+const prisma = new PrismaClient();
 
 export const ingestionWorker = new Worker(
     "ingestion",
     async (job) => {
         console.log("Received Job");
         console.log(job.data.jobId);
+        const ingestJob = await prisma.job.findUnique({
+            where: {
+                id: job.data.jobId,
+            },
+            include: {
+                dataSource: true
+            }
+        })
     },
     {
         connection: redisConfig,
